ef4bf5e2810925d64faafcbe4dd53bdc7b7139d5
Sophia <yumisuzuki3123@gmail.com>
2026-05-18 12:06:29 -0300

    Add files via upload

---
+++ programacao full stack/aula_9/servidor.js
@@ -0,0 +1,57 @@
+var http = require('http');
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+app.use(express.static('./public'));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs');
+app.set('views','/views');
+
+var usuario_cadastrado = "";
+var senha_cadastrada = "";
+
+app.get('/', function(requisicao, resposta) {
+    resposta.sendFile(path.join(__dirname, '..', 'aula_2', 'projetos.html'));
+});
+
+app.get('/cadastrar', function(requisicao, resposta) {
+    resposta.sendFile(path.join(__dirname, 'public', 'cadastro.html'));
+});
+
+app.get('/login', function(requisicao, resposta) {
+    resposta.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+
+app.post('/cadastrar', function(requisicao, resposta) {
+    usuario_cadastrado = requisicao.body.usuario;
+    senha_cadastrada = requisicao.body.senha;
+
+    resposta.render('resposta', {
+        mensagem: 'Cadastro realizado com sucesso!'
+    });
+});
+
+app.post('/entrar', function(requisicao, resposta) {
+    var usuario = requisicao.body.usuario;
+    var senha = requisicao.body.senha;
+
+    if (usuario == usuario_cadastrado && senha == senha_cadastrada) {
+        resposta.render('resposta', {
+            mensagem: 'Login realizado com sucesso!'
+        });
+    } else {
+        resposta.render('resposta', {
+            mensagem: 'Usuário ou senha incorretos!'
+        });
+    }
+});
+
+var server = http.createServer(app);
+
+server.listen(80);
+
+console.log("servidor rodando...");
