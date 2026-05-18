0f8a1d5a4540a1da4920060a15096b3c8b7f5be8
Sophia <yumisuzuki3123@gmail.com>
2026-05-18 17:02:39 -0300

    Add files via upload

--- programacao full stack/aula_10/servidor.js
+++ programacao full stack/aula_10/servidor.js
@@ -1,3 +1,4 @@
+var mongodb = require("mongodb");
 var http = require('http');
 var express = require('express');
 var path = require('path');
@@ -13,15 +14,25 @@ app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
+const MongoClient = mongodb.MongoClient;
+
+const uri = `mongodb+srv://yumisuzuki3123_db_user:V8ZpA2Pb3Ok5A0tL@banco-de-dados.vjellw1.mongodb.net/?appName=Banco-de-dados`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true });
+
+
+var dbo = client.db("Banco-de-dados");
+var posts = dbo.collection("posts");
+
+
 app.get('/', function(requisicao, resposta) {
     resposta.sendFile(path.join(__dirname, '..', 'aula_2', 'projetos.html'));
 });
 
 app.get('/blog', function(requisicao, resposta) {
-    banco.buscarPosts(function(posts) {
-        resposta.render('blog', {
-            posts: posts
-        });
+    posts.find().toArray(function(err, items) {
+        console.log(items);
+        resposta.render('blog',{posts:items})
     });
 });
 
@@ -34,9 +45,15 @@ app.post('/cadastrar_post', function(requisicao, resposta) {
     var resumo = requisicao.body.resumo;
     var conteudo = requisicao.body.conteudo;
 
-    banco.cadastrarPost(titulo, resumo, conteudo);
-
-    resposta.redirect('/blog');
+    var data = {titulo,resumo,conteudo}
+    posts.insertOne(data, function (err) {
+      if (err) {
+        console.log(err);
+        resposta.render('resposta_blog', {resposta: "Erro ao cadastrar post!"})
+      }else {
+        resposta.render('resposta_blog', {resposta: "Post cadastrado com sucesso!"})        
+      };
+    });
 });
 
 var server = http.createServer(app);
@@ -44,3 +61,5 @@ var server = http.createServer(app);
 server.listen(80);
 
 console.log("servidor rodando...");
+
+
